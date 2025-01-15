8c8ba7dab06915946a3cf3cd00a2a0e81e98d129
Mateus Reis <mateuskonige@outlook.com>
2025-01-15 14:09:53 -0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+				display: ['Aclonica', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				'primary-300': '#25fbad',
+				'secondary-900': '#34314e'
+			}
+		},
+	},
+	plugins: [],
+}
